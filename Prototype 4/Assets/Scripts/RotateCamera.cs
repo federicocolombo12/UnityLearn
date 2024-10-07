c9347fca364842d015895a1f6379f34f169a692b
federicocolombo12 <126761702+federicocolombo12@users.noreply.github.com>
2024-10-07 18:15:43 +0200

    .

---
+++ Prototype 4/Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    public float rotationSpeed;
+    private float horizontalInput
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput=Input.GetAxis("Horizontal")
+        rota
+    }
+}
