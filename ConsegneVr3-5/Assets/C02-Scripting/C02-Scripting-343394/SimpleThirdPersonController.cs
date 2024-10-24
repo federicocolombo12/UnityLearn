808d330f2e61ad3f5318a24d96b1aafc7f5cc22f
federicocolombo12 <chiccoclub5@gmail.com>
2024-10-24 16:44:49 +0200

    consegna Vr

--- ConsegneVr3-5/Assets/C02-Scripting/C02-Scripting-343394/SimpleThirdPersonController.cs
+++ ConsegneVr3-5/Assets/C02-Scripting/C02-Scripting-343394/SimpleThirdPersonController.cs
@@ -2,32 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace c02.cognome.nome
+namespace c02.colombo.federico
 {
     public class SimpleThirdPersonController : MonoBehaviour
     {
         //Which public variables do you need?
         // A Camera
+        public Camera cam;
         // A Rotation Speed
+        public float rotationSpeed = 10f;
         // A Movement Speed
-
+        public float movementSpeed = 10f;
+        private float horizontalMovement;
+        private float verticalMovement;
 
         void Update()
         {
             //Get the Input using Input.GetAxis() & assign the values to a new direction Vector3
+            horizontalMovement = Input.GetAxis("Horizontal");
+            verticalMovement = Input.GetAxis("Vertical");
+            Vector3 direction = new Vector3(horizontalMovement, 0, verticalMovement);
 
             //Compute direction According to Camera Orientation (use function TransformDirection)
-            //Reference: https://docs.unity3d.com/ScriptReference/Transform.TransformDirection.html
-
+            direction = cam.transform.TransformDirection(direction);
+            direction.y = 0.0f;
+            direction.Normalize();
             //Calculate the new direction vector between the current forward and the target direction calculated previously.
-            //To calculate it use the RotateTowards method
-            //Reference: https://docs.unity3d.com/ScriptReference/Vector3.RotateTowards.html
-
+            Vector3 newDirection = Vector3.RotateTowards(transform.forward, direction, rotationSpeed * Time.deltaTime, 0.0f);
             //Rotate the object, you can use Quaternion.LookRotation() function.
-            //Reference: https://docs.unity3d.com/ScriptReference/Quaternion.LookRotation.html
-
+            transform.rotation = Quaternion.LookRotation(newDirection);
             //Translate along forward
-
+            transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
         }
     }
 }
